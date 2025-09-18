4975a9914b36a4b0c9db740f89c5807b2712eccf
Gerry0303 <imanuelgerry0303@gmail.com>
2025-09-18 17:44:23 +0700

    fix: Update baseURL in http helper to use secure HTTPS endpoint

--- client-folder/src/helpers/http.jsx
+++ client-folder/src/helpers/http.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://ip.gerrygurusinga.xyz/api/v1',
 });
 
 export default http;
